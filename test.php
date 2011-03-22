6c10765f37fa3e2a052b0cc127c482d3f56fc99f
Ruslan Rybak <ruslan.rybak@gmail.com>
2011-03-22 16:33:47 +0100

    one more file

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ruslan
+ * Date: 22.03.11
+ * Time: 15:58
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'one more test on git';
+
+ 
